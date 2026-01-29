5eea6c1e3b791ee1c8f9ece8686b183d941388df
lovelyMai <lovelymai5228@icloud.com>
2026-01-29 14:46:34 +0800

    移除了多余的文件跟踪

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import '@/assets/base.css'
-import '@/assets/main.css'
-import '@/assets/icons/iconfont.css'
-
-import App from './App.vue'
-import { createApp } from 'vue'
-
-createApp(App).mount('#app')
