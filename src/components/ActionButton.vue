42725a55acfd72dd87f2acf54ac460b1cd4f8443
lovelyMai <lovelymai5228@icloud.com>
2026-02-02 16:42:43 +0800

    修复弹窗

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -29,49 +29,55 @@ const clickIcon = (e: any) => {
 </script>
 
 <template>
-  <span class="ActionButton">
-    <span class="iconfont icon-info" @click.stop="clickIcon" style="user-select: none;">
-      <ul v-if="isPop" :style="{ left: `${popX}px`, top: `${popY}px` }">
-        <li v-for="(item, index) in props.list" :key="index" @click="props.onItemClick?.(index)">
-          <slot :item="item" :index="index">{{ item }}</slot>
-        </li>
-      </ul>
-    </span>
+  <span class="iconfont icon-info ActionButton" @click.stop="clickIcon" style="user-select: none;">
+    <ul v-if="isPop" :style="{ left: `${popX}px`, top: `${popY}px` }">
+      <li v-for="(item, index) in props.list" :key="index" @click="props.onItemClick?.(index)">
+        <slot :item="item" :index="index"></slot>
+        <span class="name">{{ item }}</span>
+      </li>
+    </ul>
   </span>
 </template>
 
 <style scoped>
-.icon-info {
+.ActionButton {
   display: inline-block;
   position: relative;
   font-size: 20px;
   color: #3b86f7;
   cursor: pointer;
   --list-z-index: 2;
+  --list-font-size: 10px
 }
 
 ul {
   position: absolute;
   z-index: var(--list-z-index);
-  width: 60px;
   padding: 2px;
   background-color: #f3f6f6;
   border-radius: 10px;
   box-shadow: 0 0 10px 0 rgba(0, 0, 0, .1);
+  transform: translateZ(0);
+  backface-visibility: hidden;
 }
 
 li {
   display: flex;
-  justify-content: center;
   height: 26px;
+  padding: 0 5px;
   border-radius: 8px;
-  font-size: 14px;
+  font-size: var(--list-font-size);
   color: #000;
   line-height: 26px;
+  white-space: nowrap;
 }
 
 li:hover {
   background-color: #3b86f7;
   color: #fff;
+}
+
+.name {
+  margin-left: 4px;
 }
 </style>
