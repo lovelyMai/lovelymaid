8b19b6f03f8ba86695bbc0727e3649635d93d87f
lovelyMai <lovelymai5228@icloud.com>
2026-03-21 01:33:33 +0800

    TabBar优化

--- src/components/TabBar.vue
+++ src/components/TabBar.vue
@@ -15,10 +15,10 @@ interface Props {
   /** 是否启用搜索按钮 */
   showSearch?: boolean
   /** 挂载时激活索引 */
-  index?: number
+  initialIndex?: number
 }
 const props = withDefaults(defineProps<Props>(), {
-  index: 0
+  initialIndex: 0
 })
 const listLength = computed(() => props.list.length)
 
@@ -35,8 +35,9 @@ const maxDistance = ref<number>(0)
 const virtualLeft = ref<number>(0)
 const virtualRight = ref<number>(0)
 const barTransition = ref<string>('none')
-const activeIndex = ref<number>(props.index)
-const realPos = ref<number>(0)
+const activeIndex = ref<number>(props.initialIndex)
+const targetPos = ref<number>(0)
+let slideCenter: number
 onMounted(() => {
   cleanup = watchRef(barRef, () => {
     barHeight.value = barRef.value!.offsetHeight
@@ -47,7 +48,8 @@ onMounted(() => {
     virtualLeft.value = activeIndex.value * slideWidth.value
     virtualRight.value = virtualLeft.value + slideWidth.value
     maxDistance.value = (barWidth.value - 6) * (listLength.value - 1) / listLength.value
-    realPos.value = activeIndex.value * slideWidth.value
+    targetPos.value = activeIndex.value * slideWidth.value
+    slideCenter = targetPos.value + slideWidth.value / 2
   }, true)
   setTimeout(() => barTransition.value = 'width .3s, height .3s, transform .2s', 500)
 })
@@ -78,7 +80,8 @@ const barBackgroundColor = ref<string>('rgba(248, 248, 248, 0.9)')
 const border = ref<string>('none')
 const boxShadow = ref<string>('none')
 const slideScale = ref<number>(1)
-const slideTransition = ref<string>('background-color .1s')
+const textScale = computed<number>(() => slideScale.value === 1.2 ? 1.1 : 1)
+const slideTransition = ref<string>('background .1s')
 
 let followed = false
 const calculateTargetPos = (e: PointerEvent, type: 'start' | 'move') => {
@@ -88,39 +91,51 @@ const calculateTargetPos = (e: PointerEvent, type: 'start' | 'move') => {
   const realLeft = new DOMMatrix(window.getComputedStyle(slideRef.value).transform).m41
   const targetLeft = clickLeft < 0 ? 0 : clickLeft > (listLength.value - 1) * slideRef.value.offsetWidth ? (listLength.value - 1) * slideRef.value.offsetWidth : clickLeft
   if (type === 'start') {
-    slideTransition.value = `transform .5s, background-color .1s`
-    realPos.value = targetLeft
+    slideTransition.value = `transform .5s, background .1s`
+    targetPos.value = targetLeft
   } else if (followed || Math.abs(targetLeft - realLeft) < (maxDistance.value / 5)) {
-    slideTransition.value = `background-color .1s`
-    realPos.value = targetLeft
+    slideTransition.value = `background .1s`
+    targetPos.value = targetLeft
     followed = true
   } else {
-    slideTransition.value = `background-color .1s`
-    realPos.value = realLeft
+    slideTransition.value = `background .1s`
+    targetPos.value = realLeft
     calculatePosTimer = setInterval(() => {
       const realLeft = new DOMMatrix(window.getComputedStyle(slideRef.value!).transform).m41
       if (Math.abs(targetLeft - realLeft) < (maxDistance.value / 5)) {
-        slideTransition.value = `background-color .1s`
-        realPos.value = targetLeft
+        slideTransition.value = `background .1s`
+        targetPos.value = targetLeft
         followed = true
         clearTimer(calculatePosTimer)
       } else {
-        realPos.value += (targetLeft - realLeft) > 0 ? maxDistance.value / 5 : -maxDistance.value / 5
+        targetPos.value += (targetLeft - realLeft) > 0 ? maxDistance.value / 5 : -maxDistance.value / 5
       }
     }, 8)
   }
 }
 const updateVirtualPos = (duration: number | undefined) => {
-  const containerLeft = getLayoutLeftInViewport(containerRef.value)
+  const calc = () => {
+    const realLeft = new DOMMatrix(window.getComputedStyle(slideRef.value!).transform).m41
+    slideCenter = realLeft + slideWidth.value / 2
+    const virtualWidth = slideWidth.value * 1.2 / 1.1
+    virtualLeft.value = slideCenter - virtualWidth / 2
+    virtualRight.value = slideCenter + virtualWidth / 2
+  }
   if (duration) {
+    clearInterval(virtualTimer1)
     virtualTimer1 = setInterval(() => {
-      virtualLeft.value = slideRef.value!.getBoundingClientRect().left - containerLeft
-      virtualRight.value = slideRef.value!.getBoundingClientRect().right - containerLeft
+      calc()
     }, 8)
     virtualTimer2 = setTimeout(() => clearInterval(virtualTimer1), duration)
   } else {
-    virtualLeft.value = slideRef.value!.getBoundingClientRect().left - containerLeft
-    virtualRight.value = slideRef.value!.getBoundingClientRect().right - containerLeft
+    clearInterval(virtualTimer1)
+    if (followed) {
+      calc()
+    } else {
+      virtualTimer1 = setInterval(() => {
+        calc()
+      }, 8)
+    }
   }
 }
 
@@ -128,12 +143,12 @@ let startTime: number
 const startSlide = (e: PointerEvent) => {
   startTime = Date.now()
   backgroundTimer = setTimeout(() => {
-    background.value = `transparent`
+    background.value = '#fff'
   }, 100)
   barBackgroundColor.value = '#fff'
   border.value = '1px solid rgba(255, 255, 255, 0.5)'
   boxShadow.value = '0 0 10px 0 rgba(0, 0, 0, 0.1)'
-  slideScale.value = 1.4
+  slideScale.value = 1.2
   calculateTargetPos(e, 'start')
   clearTimer(virtualTimer1, virtualTimer2)
   updateVirtualPos(500)
@@ -155,7 +170,7 @@ const stopSlide = (e: PointerEvent) => {
   barBackgroundColor.value = 'rgba(248, 248, 248, 0.9)'
   border.value = 'none'
   boxShadow.value = 'none'
-  slideTransition.value = `transform .5s, background-color .1s`
+  slideTransition.value = `transform .5s, background .1s`
   slideScale.value = 1
   if (searchIsActive.value) {
     searchIsActive.value = false
@@ -165,7 +180,7 @@ const stopSlide = (e: PointerEvent) => {
     activeIndex.value = index > listLength.value - 1 ? listLength.value - 1 : index < 0 ? 0 : index
   }
   clearTimer(calculatePosTimer)
-  realPos.value = activeIndex.value * slideWidth.value
+  targetPos.value = activeIndex.value * slideWidth.value
   followed = false
   clearTimer(virtualTimer1, virtualTimer2)
   updateVirtualPos(500)
@@ -175,14 +190,14 @@ const stopSlide = (e: PointerEvent) => {
 }
 
 // list改变归位
-watch(() => props.list.length, () => realPos.value = 0)
+watch(() => props.list.length, () => targetPos.value = 0)
 
 // 模式切换
 const searchIsActive = ref<boolean>(false)
 const searchIsShow = ref<boolean>(false)
 const clickSearch = () => {
   searchIsActive.value = true
-  realPos.value = 0
+  targetPos.value = 0
 }
 watch(searchIsActive, (newValue) => {
   if (newValue) setTimeout(() => searchIsShow.value = true, 100)
@@ -194,7 +209,7 @@ defineExpose({
   clickTab: (index: number) => {
     activeIndex.value = index
     clearTimer(calculatePosTimer)
-    realPos.value = index * slideWidth.value
+    targetPos.value = index * slideWidth.value
     clearTimer(virtualTimer1, virtualTimer2)
     updateVirtualPos(500)
     props.onItemClick?.(props.list[index], index)
@@ -256,6 +271,10 @@ defineExpose({
   transition: v-bind(barTransition);
 }
 
+.bar:active {
+  transform: scale(1.05);
+}
+
 .bar:not(.active):active {
   transform: scale(1.2);
 }
@@ -290,8 +309,10 @@ defineExpose({
   left: 0;
   top: 0;
   z-index: 1;
-  clip-path: inset(0 calc(100% - v-bind(virtualRight) * 1px) 0 calc(v-bind(virtualLeft) * 1px) round 22px);
-  transform: translateZ(0);
+  clip-path: inset(0 calc(100% - v-bind(virtualRight) * 1px) 0 calc(v-bind(virtualLeft) * 1px) round calc((v-bind(borderRadius) - 3) * 1px));
+  transform: translateZ(0) scale(v-bind(textScale));
+  transform-origin: calc(v-bind(slideCenter) * 1px) center;
+  transition: transform .2s;
   backface-visibility: hidden;
 }
 
@@ -343,7 +364,7 @@ defineExpose({
   border: v-bind(border);
   border-radius: calc((v-bind(borderRadius) - 3) * 1px);
   box-shadow: v-bind(boxShadow);
-  transform: translateX(calc(v-bind(realPos) * 1px)) scale(v-bind(slideScale));
+  transform: translateX(calc(v-bind(targetPos) * 1px)) scale(v-bind(slideScale));
   transition: v-bind(slideTransition);
 }
 </style>
